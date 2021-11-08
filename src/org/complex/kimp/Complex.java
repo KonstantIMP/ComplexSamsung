e8ae04d0c230eb4bae3ec70478ff4085a4605912
KonstantIMP <mihedovkos@gmail.com>
2021-11-08 10:19:07 +0300

    Add Complex ctors

---
+++ src/org/complex/kimp/Complex.java
@@ -0,0 +1,22 @@
+package org.complex.kimp;
+
+/**
+ * Describes complex number
+ */
+public class Complex {
+    private double real;
+    private double imag;
+
+    public Complex() {
+        real = imag = 0.0;
+    }
+
+    public Complex(double real, double imag){
+        this.real = real; this.imag = imag;
+    }
+
+    public Complex(Complex c){
+        this.real = c.real;
+        this.imag = c.imag;
+    }
+}
