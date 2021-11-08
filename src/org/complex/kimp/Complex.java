f1746032ee1be429f0f7d61a208ee924b02df24e
KonstantIMP <mihedovkos@gmail.com>
2021-11-08 10:35:11 +0300

    Add add sum sub and mul

--- src/org/complex/kimp/Complex.java
+++ src/org/complex/kimp/Complex.java
@@ -27,4 +27,23 @@ public class Complex {
     public double getReal() {
         return real;
     }
+
+    public void add(Complex c){
+        this.real += c.real;
+        this.imag += c.imag;
+    }
+
+    public static Complex sum(Complex c1, Complex c2){
+        return new Complex(c1.real + c2.real, c1.imag + c2.imag);
+    }
+
+    public static Complex sub(Complex c1, Complex c2){
+        return new Complex(c1.real - c2.real, c1.imag - c2.imag);
+    }
+
+    public static Complex mul(Complex c1, Complex c2){
+        double r = c1.real * c2.real - c1.imag * c2.imag;
+        double i = c1.imag * c2.real + c2.imag * c1.real;
+        return new Complex(r, i);
+    }
 }
